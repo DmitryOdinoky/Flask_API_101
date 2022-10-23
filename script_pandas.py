d990cd03096423240d8ee9e7f2e0eafdc88e82d9
dmitrijs.odinokijs <dmitrijs.odinokijs@sonarworks.com>
2022-10-23 19:54:49 +0300

    done

--- script_pandas.py
+++ script_pandas.py
@@ -16,11 +16,23 @@ st = time.time()
 def process_csv(filename):
     
     # product_types = defaultdict(Decimal)
-
-    df = pd.read_csv(my_absolute_dirpath + "/input/" + filename, index_col=False)
-    df_out = df.groupby(["Song", "Date"])["Number of Plays"].sum().reset_index().sort_values(by=['Song'], ascending=False) 
-    # output_file = filename
-    df_out.to_csv(my_absolute_dirpath + "/output/" + filename, index=False)
+    
+        
+    df_iter = pd.read_csv(my_absolute_dirpath + "/input/" + filename, index_col=False, chunksize=500)
+    
+    for i, df_out in enumerate(df_iter):
+    
+        df_out = df_out.groupby(["Song", "Date"])["Number of Plays"].sum().reset_index().sort_values(by=['Song'], ascending=False) 
+        # output_file = filename
+        
+        # Set writing mode to append after first chunk
+        mode = 'w' if i == 0 else 'a'
+        
+        # Add header if it is the first chunk
+        header = i == 0
+        
+        df_out.to_csv(my_absolute_dirpath + "/output/" + filename, index=False, header=header, mode=mode)
+    
     
     et = time.time()
     elapsed_time = round(et - st, 2)
